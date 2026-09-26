c6bb75356ae75e5b88560ceeac7f631365002ebb
Anish Venkatesalu <anivcs50@gmail.com>
2026-09-26 13:06:25 -0700

    feat: showcase the top 20 movies using jQuery and HTML.

---
+++ WebContent/index.js
@@ -0,0 +1,57 @@
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the HTML skeleton is created.
+ *
+ * This .js performs two steps:
+ *      1. Use jQuery to talk to backend API to get the JSON data.
+ *      2. Populate the data to correct HTML elements.
+ */
+
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into HTML elements
+ * @param resultData jsonObject
+ */
+function handleMoviesResult(resultData) {
+    console.log("handleMoviesResult: populating movies table from resultData");
+
+    // Populate the star table
+    // Find the empty table body by id "movie_table_body"
+    let starTableBodyElement = jQuery("#movie_table_body");
+
+    // Iterate through resultData, no more than 20 entries
+    for (let i = 0; i < Math.min(20, resultData.length); i++) {
+
+        // Concatenate the HTML tags with resultData jsonObject
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML +=
+            "<th>" +
+            // Add a link to single-movie.html with id passed with GET url parameter
+            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
+            + resultData[i]["movie_title"] +     // display movie_title for the link text
+            '</a>' +
+            "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        starTableBodyElement.append(rowHTML);
+    }
+}
+
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movies", // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMoviesResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
